55fe5e2039e250f8ff83d6bb2172e3224a4e00c1
Maria1504bu <15504420011m@gmail.com>
2022-10-01 13:17:33 +0300

    insert demo values

--- src/main/webapp/db/insert_values.sql
+++ src/main/webapp/db/insert_values.sql
@@ -1,11 +1,19 @@
-
-INSERT INTO roles (name) VALUES ('ADMIN'), ('CANDIDATE');
-INSERT INTO languages (name, lang_code) VALUES ('English', 'en'), ('Українська', 'uk');
-INSERT INTO logins (email, password, role_id) VALUES ('admin@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 1),
-                                                     ('candidate@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
-                                                     ('vova27@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
-                                                     ('grabovych@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2);
-INSERT INTO cities (`name`)
+INSERT INTO roles (name)
+VALUES ('ADMIN'),
+       ('CANDIDATE');
+INSERT INTO languages (name, lang_code)
+VALUES ('English', 'en'),
+       ('Українська', 'uk');
+INSERT INTO logins (email, password, role_id)
+VALUES ('admin@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 1),
+       ('candidate@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
+       ('vova27@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
+       ('grabovych@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
+       ('kozarDiana@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
+       ('kravchukDmytro@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2),
+       ('sapushakRoman@gmail.com', 'AAB27D62AA18F1D428D488788A0B1BEC', 2);
+INSERT
+INTO cities (`name`)
 VALUES ('Cherkasy'),
        ('Chernihiv'),
        ('Chernivtsi'),
@@ -30,35 +38,128 @@ VALUES ('Cherkasy'),
        ('Zakarpattia'),
        ('Zaporizhia'),
        ('Zhytomyr');
-INSERT INTO candidates VALUES(2, 'Dudar', 'Sofia', 'Stepanivna', 'havenot', 3,
-                              'school 23', FALSE, '2022-08-14'),
-                           (3, 'Futka', 'Volodymyr', 'Mykolayovych', 'havenot', 6,
-                            'school 17', FALSE, '2022-08-29'),
-                           (4, 'Grabovych', 'Petro', 'Bogdanovych', 'havenot', 10,
-                            'school 22', FALSE, '2022-08-29');
-INSERT INTO faculties (budget_places, total_places) VALUES (4, 10), (5, 7), (6, 8);
-INSERT INTO faculties_languages (faculty_id, language_id, name) VALUES (1, 1, 'Management'),
-                                                                          (1, 2, 'Менеджмент'),
-                                                                       (2, 1, 'Audit'),
-                                                                       (2, 2, 'Аудит'),
-                                                                       (3, 1, 'Engineering'),
-                                                                       (3, 2, 'Машинобудування');
-INSERT INTO applications (login_id, faculty_id, status) VALUES (2, 1, 'NOT_PROCEED'),
-                                                                         (2, 2, 'NOT_PROCEED'),
-                                                                         (2, 3, 'NOT_PROCEED'),
-                                                                         (3, 1, 'NOT_PROCEED'),
-                                                                         (3, 2, 'NOT_PROCEED'),
-                                                                         (4, 1, 'NOT_PROCEED');
-INSERT INTO subjects (maxGrade) VALUES (150), (140), (45), (40);
+INSERT INTO candidates
+VALUES (2, 'Дудар', 'Софія', 'Степанівна', 'havenot', 3,
+        'school 23', FALSE, '2022-08-14'),
+       (3, 'Футка', 'Володимир', 'Миколайович', 'havenot', 6,
+        'school 17', FALSE, '2022-08-29'),
+       (4, 'Грабович', 'Петро', 'Богданович', 'havenot', 10,
+        'school 22', FALSE, '2022-08-29'),
+       (5, 'Козар', 'Діана', 'Петрівна', 'havenot', 12,
+        'school 22', FALSE, '2022-09-21'),
+       (6, 'Кравчук', 'Дмитро', 'Остапович', 'havenot', 2,
+        'school 2', FALSE, '2022-09-21'),
+       (7, 'Сапужак', 'Роман', 'Олександрович', 'havenot', 19,
+        'school 5', FALSE, '2022-09-21');
+INSERT INTO faculties (budget_places, total_places)
+VALUES (4, 10),
+       (2, 7),
+       (6, 8);
+INSERT INTO faculties_languages (faculty_id, language_id, name)
+VALUES (1, 1, 'Management'),
+       (1, 2, 'Менеджмент'),
+       (2, 1, 'Audit'),
+       (2, 2, 'Аудит'),
+       (3, 1, 'Engineering'),
+       (3, 2, 'Машинобудування');
+INSERT INTO applications (login_id, faculty_id, status)
+VALUES (2, 1, 'NOT_PROCEED'),
+       (2, 2, 'NOT_PROCEED'),
+       (2, 3, 'NOT_PROCEED'),
+       (3, 1, 'NOT_PROCEED'),
+       (3, 2, 'NOT_PROCEED'),
+       (4, 1, 'NOT_PROCEED'),
+       (5, 1, 'NOT_PROCEED'),
+       (5, 2, 'NOT_PROCEED'),
+       (5, 3, 'NOT_PROCEED'),
+       (6, 1, 'NOT_PROCEED'),
+       (6, 3, 'NOT_PROCEED'),
+       (7, 2, 'NOT_PROCEED'),
+       (7, 3, 'NOT_PROCEED');
+INSERT INTO subjects (maxGrade)
+VALUES (150),
+       (140),
+       (45),
+       (40);
 
-INSERT INTO subjects_languages VALUES (1, 1, 'Math'), (1, 2, 'Математика'),
-                                      (2, 1, 'Chemistry'), (2, 2, 'Хімія'),
-                                      (3, 1, 'Biology'), (3, 2, 'Біологія'),
-                                      (4, 1, 'English'), (4, 2, 'Англійська мова');
-INSERT INTO faculties_subjects VALUES (1, 1),
-                                      (1, 4),
-                                      (2, 1),
-                                      (3, 1),
-                                      (3, 2);
-INSERT INTO grades (subject_id, grade) VALUES (1, 123), (2, 147);
-INSERT INTO applications_grades VALUES (1, 1), (1, 2), (2, 2), (3, 1), (3, 2);
+INSERT INTO subjects_languages
+VALUES (1, 1, 'Math'),
+       (1, 2, 'Математика'),
+       (2, 1, 'Chemistry'),
+       (2, 2, 'Хімія'),
+       (3, 1, 'Physic'),
+       (3, 2, 'Фізика'),
+       (4, 1, 'English'),
+       (4, 2, 'Англійська мова');
+INSERT INTO faculties_subjects
+VALUES (1, 1, 40),
+       (1, 4, 60),
+       (2, 1, 70),
+       (2, 2, 30),
+       (3, 2, 20),
+       (3, 3, 30),
+       (3, 4, 50);
+INSERT INTO faculties_subjects
+VALUES (1, 3, 40);
+INSERT INTO grades (subject_id, grade)
+VALUES (1, 123),
+       (4, 32),
+       (1, 112),
+       (2, 112),
+       (2, 120),
+       (3, 98),
+       (4, 40),
+       (1, 100),
+       (4, 22),
+       (1, 150),
+       (2, 140),
+       (1, 147),
+       (4, 35),
+       (1, 111),
+       (4, 34),
+       (1, 74),
+       (2, 77),
+       (2, 140),
+       (3, 39),
+       (4, 36),
+       (1, 88),
+       (4, 30),
+       (2, 134),
+       (3, 33),
+       (4,40),
+       (1, 77),
+       (2, 26),
+       (2, 60),
+       (3, 34),
+       (4, 27);
+INSERT INTO applications_grades
+VALUES (1, 1),
+       (1, 2),
+       (2, 3),
+       (2, 4),
+       (3, 5),
+       (3, 6),
+       (3, 7),
+       (4, 8),
+       (4, 9),
+       (5, 10),
+       (5, 11),
+       (6, 12),
+       (6, 13),
+       (7, 14),
+       (7, 15),
+       (8, 16),
+       (8, 17),
+       (9, 18),
+       (9, 19),
+       (9, 20),
+       (10, 21),
+       (10, 22),
+       (11, 23),
+       (11, 24),
+       (11, 25),
+       (12, 26),
+       (12, 27),
+       (13, 28),
+       (13, 29),
+       (13, 30);
